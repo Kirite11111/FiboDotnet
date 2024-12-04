c6b06f7839f97f752714e87dbade084d3c7e719d
Kirite <eliott.garnier2810@gmail.com>
2024-12-04 16:49:50 +0100

    feat: add bdd link and solution save

--- src/Leonardo/Fibonacci.cs
+++ src/Leonardo/Fibonacci.cs
@@ -1,4 +1,6 @@
-﻿namespace Leonardo;
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Leonardo;
 
 public record FibonacciResult(int Input, int Result);
 
@@ -13,28 +15,49 @@ public static class Fibonacci
     }
     public static async Task<List<FibonacciResult>> RunAsync(string[] strings)
     {
-        
+        await using var context = new FibonacciDataContext();
         
         var tasks = new List<Task<FibonacciResult>>();
         foreach (var input in strings)
         {
             
             var int32 = Convert.ToInt32(input);
-            var r = Task.Run(() =>
+            
+            var t_fibo = await context.TFibonaccis.Where(t => t.FibId == int32).FirstOrDefaultAsync();
+
+            if (t_fibo != null)
             {
-                var result = Fibonacci.Run(int32);
-                return new FibonacciResult(int32, result);
-            });
-            tasks.Add(r);
+                var t = Task.Run(() =>
+                {
+                    return new FibonacciResult(t_fibo.FibInput, t_fibo.FibOutput);
+                });
+                tasks.Add(t);
+            }
+            else
+            {
+
+                var r = Task.Run(() =>
+                {
+                    var result = Fibonacci.Run(int32);
+                    return new FibonacciResult(int32, result);
+                });
+                tasks.Add(r);
+            }
         }
     
         var results = new List<FibonacciResult>();
         foreach (var task in tasks)
         {
             var r = await task;
+            context.TFibonaccis.Add(new TFibonacci
+                {
+                    FibInput = r.Input,
+                    FibOutput = r.Result
+                }
+            );
             results.Add(r);
         }
-    
+        await context.SaveChangesAsync();
         return results;
     }
 }
